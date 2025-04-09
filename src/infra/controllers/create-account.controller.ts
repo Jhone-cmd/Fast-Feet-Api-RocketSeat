ac1d295a2db95beed802f3cf99192ef437fe00ea
Jhone-cmd <jhone.1941@gmail.com>
2025-04-09 19:56:34 -0300

    chore: Adjustment in importing the create-account.controller.ts dependency and generating password hash

--- src/infra/controllers/create-account.controller.ts
+++ src/infra/controllers/create-account.controller.ts
@@ -1,32 +1,40 @@
-import { Body, ConflictException, Controller, HttpCode, Post } from "@nestjs/common";
-import type { PrismaService } from "../prisma/prisma.service";
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { PrismaService } from '../prisma/prisma.service'
 
 @Controller('/accounts')
 export class CreateAccountController {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    @Post()
-    @HttpCode(201)
-    async handle(@Body() body: any) {
-        const { name, email, cpf, password } = body
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: any) {
+    const { name, email, cpf, password } = body
 
-        const AccountEmailSameWithExists = await this.prisma.account.findUnique({
-            where: {
-                email
-            }
-        })
+    const AccountSameWithExistEmail = await this.prisma.accounts.findUnique({
+      where: {
+        email,
+      },
+    })
 
-        if (AccountEmailSameWithExists) {
-            throw new ConflictException('Email already exists')
-        }
-
-        await this.prisma.account.create({
-            data: {
-                name, 
-                email,
-                cpf,
-                password
-            }
-        })
+    if (AccountSameWithExistEmail) {
+      throw new ConflictException('Email already exists')
     }
+
+    const hashPassword = await hash(password, 8)
+    await this.prisma.accounts.create({
+      data: {
+        name,
+        email,
+        cpf,
+        password: hashPassword,
+      },
+    })
+  }
 }
