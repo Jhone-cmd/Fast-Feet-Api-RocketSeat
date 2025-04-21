59ff1574bf83cab4c764179f47ecf3e471816947
Jhone-cmd <jhone.1941@gmail.com>
2025-04-21 10:58:19 -0300

    chore: e2e account testing

--- src/infra/controllers/authenticate.controller.e2e-spec.ts
+++ src/infra/controllers/authenticate.controller.e2e-spec.ts
@@ -1,10 +1,11 @@
 import { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
 import request from 'supertest'
 import { AppModule } from '../app.module'
 import { PrismaService } from '../prisma/prisma.service'
 
-describe('Create Account (E2E)', () => {
+describe('Authenticate (E2E)', () => {
   let app: INestApplication
   let prisma: PrismaService
 
@@ -18,22 +19,25 @@ describe('Create Account (E2E)', () => {
     await app.init()
   })
 
-  test('[POST] /accounts', async () => {
-    const response = await request(app.getHttpServer()).post('/accounts').send({
-      name: 'deliveryman',
-      email: 'deliveryman@email.com',
-      cpf: '12345678901',
+  test('[POST] /sessions', async () => {
+    await prisma.accounts.create({
+      data: {
+        name: 'admin',
+        email: 'admin@email.com',
+        cpf: '12345678900',
+        password: await hash('123456', 8),
+        role: 'ADMIN',
+      },
+    })
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      cpf: '12345678900',
       password: '123456',
     })
 
     expect(response.statusCode).toBe(201)
-
-    const accountOnDatabase = await prisma.accounts.findUnique({
-      where: {
-        cpf: '12345678901',
-      },
+    expect(response.body).toEqual({
+      access_token: expect.any(String),
     })
-
-    expect(accountOnDatabase).toBeTruthy()
   })
 })
