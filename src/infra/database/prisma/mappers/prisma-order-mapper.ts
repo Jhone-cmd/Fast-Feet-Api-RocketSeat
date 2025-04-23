4837e83018da1aa4a76614573b9b7fd1096772ec
Jhone-cmd <jhone.1941@gmail.com>
2025-04-23 15:42:45 -0300

    chore: conversation between layers (mappers) - prisma-order-mapper

---
+++ src/infra/database/prisma/mappers/prisma-order-mapper.ts
@@ -0,0 +1,38 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Order } from '@/domain/fast-feet/enterprise/entities/order'
+import { Slug } from '@/domain/fast-feet/enterprise/entities/value-objects/slug'
+import { Prisma, Orders as PrismaOrder } from '@prisma/client'
+
+export class PrismaOrderMapper {
+  static toDomain(raw: PrismaOrder): Order {
+    return Order.create(
+      {
+        name: raw.name,
+        slug: Slug.create(raw.slug),
+        recipientId: new UniqueEntityId(raw.recipient_id),
+        deliverymanId: raw.deliveryman_id
+          ? new UniqueEntityId(raw.deliveryman_id)
+          : null,
+        status: raw.status,
+        latitude: raw.latitude.toNumber(),
+        longitude: raw.longitude.toNumber(),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityId(raw.id)
+    )
+  }
+
+  static toPrisma(order: Order): Prisma.OrdersUncheckedCreateInput {
+    return {
+      id: order.id.toString(),
+      name: order.name,
+      slug: order.slug.toString(),
+      recipient_id: order.recipientId.toString(),
+      deliveryman_id: order.deliverymanId?.toString(),
+      status: order.status,
+      latitude: order.latitude,
+      longitude: order.longitude,
+    }
+  }
+}
--- src/infra/database/prisma/repositories/prisma-order-repository.ts
+++ src/infra/database/prisma/repositories/prisma-order-repository.ts
@@ -3,25 +3,98 @@ import { PaginationParams } from '@/core/repositories/pagination-params'
 import { OrderRepository } from '@/domain/fast-feet/application/repositories/order-repository'
 import { Order } from '@/domain/fast-feet/enterprise/entities/order'
 import { Injectable } from '@nestjs/common'
+import { PrismaOrderMapper } from '../mappers/prisma-order-mapper'
+import { PrismaService } from '../prisma.service'
 
 @Injectable()
 export class PrismaOrderRepository implements OrderRepository {
-  create(order: Order): Promise<void> {
-    throw new Error('Method not implemented.')
+  constructor(private prisma: PrismaService) {}
+
+  async create(order: Order): Promise<void> {
+    const data = PrismaOrderMapper.toPrisma(order)
+    await this.prisma.orders.create({
+      data,
+    })
   }
-  findManyOrder(params: PaginationParams): Promise<Order[]> {
-    throw new Error('Method not implemented.')
+
+  async findManyOrder({ page }: PaginationParams): Promise<Order[]> {
+    const perPage = 20
+    const orders = await this.prisma.orders.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return orders.map(PrismaOrderMapper.toDomain)
   }
-  findById(id: string): Promise<Order | null> {
-    throw new Error('Method not implemented.')
+
+  async findById(id: string): Promise<Order | null> {
+    const order = await this.prisma.orders.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!order) return null
+
+    return PrismaOrderMapper.toDomain(order)
   }
-  findNearbyOrders(params: FindNearbyOrdersParams): Promise<Order[]> {
-    throw new Error('Method not implemented.')
+
+  async findNearbyOrders({
+    page,
+    latitude,
+    longitude,
+  }: FindNearbyOrdersParams): Promise<Order[]> {
+    const perPage = 20
+    const orders = await this.prisma.orders.findMany({
+      where: {
+        AND: [
+          {
+            latitude: {
+              gte: latitude - 10 / 111.32,
+              lte: latitude + 10 / 111.32,
+            },
+          },
+          {
+            longitude: {
+              gte:
+                longitude -
+                10 / (111.32 * Math.cos((latitude * Math.PI) / 180)),
+              lte:
+                longitude +
+                10 / (111.32 * Math.cos((latitude * Math.PI) / 180)),
+            },
+          },
+        ],
+      },
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return orders.map(PrismaOrderMapper.toDomain)
   }
-  save(order: Order): Promise<void> {
-    throw new Error('Method not implemented.')
+
+  async save(order: Order): Promise<void> {
+    const data = PrismaOrderMapper.toPrisma(order)
+    await this.prisma.orders.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    })
   }
-  delete(order: Order): Promise<void> {
-    throw new Error('Method not implemented.')
+
+  async delete(order: Order): Promise<void> {
+    const data = PrismaOrderMapper.toPrisma(order)
+    await this.prisma.orders.delete({
+      where: {
+        id: data.id,
+      },
+    })
   }
 }
