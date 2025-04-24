58332ca9a0511d780cfcdb9571abab246d1adbd9
Jhone-cmd <jhone.1941@gmail.com>
2025-04-24 16:05:16 -0300

    chore: conversation between layers (mappers) - prisma-attachment-mapper

--- src/infra/database/prisma/repositories/prisma-attachment-repository.ts
+++ src/infra/database/prisma/repositories/prisma-attachment-repository.ts
@@ -1,10 +1,17 @@
 import { AttachmentRepository } from '@/domain/fast-feet/application/repositories/attachment-repository'
 import { Attachment } from '@/domain/fast-feet/enterprise/entities/attachment'
 import { Injectable } from '@nestjs/common'
+import { PrismaAttachmentMapper } from '../mappers/prisma-attachment-mapper'
+import { PrismaService } from '../prisma.service'
 
 @Injectable()
 export class PrismaAttachmentRepository implements AttachmentRepository {
-  create(attachment: Attachment): Promise<void> {
-    throw new Error('Method not implemented.')
+  constructor(private prisma: PrismaService) {}
+
+  async create(attachment: Attachment): Promise<void> {
+    const data = PrismaAttachmentMapper.toPrisma(attachment)
+    await this.prisma.attachments.create({
+      data,
+    })
   }
 }
