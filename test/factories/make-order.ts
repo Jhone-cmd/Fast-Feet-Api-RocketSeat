a25f68262759444e73bf6c9905bce7e101fc7e6b
Jhone-cmd <jhone.1941@gmail.com>
2025-05-06 10:07:53 -0300

    chore: creating factory for order

--- test/factories/make-order.ts
+++ test/factories/make-order.ts
@@ -3,7 +3,10 @@ import {
   Order,
   type OrderProps,
 } from '@/domain/fast-feet/enterprise/entities/order'
+import { PrismaOrderMapper } from '@/infra/database/prisma/mappers/prisma-order-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeOrder(
   override: Partial<OrderProps> = {},
@@ -22,4 +25,18 @@ export function makeOrder(
   )
 
   return order
+}
+@Injectable()
+export class OrderFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaOrder(data: Partial<OrderProps> = {}): Promise<Order> {
+    const order = makeOrder(data)
+
+    await this.prisma.orders.create({
+      data: PrismaOrderMapper.toPrisma(order),
+    })
+
+    return order
+  }
 }
