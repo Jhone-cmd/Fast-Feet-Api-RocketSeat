9c31b6bbf67b20e22693735e3b4c1e37f39fb171
Jhone-cmd <jhone.1941@gmail.com>
2025-05-06 09:59:32 -0300

    chore: creating factory for account and refactoring authentication test

--- src/infra/http/controllers/authenticate.controller.e2e-spec.ts
+++ src/infra/http/controllers/authenticate.controller.e2e-spec.ts
@@ -1,33 +1,34 @@
+import { CPF } from '@/domain/fast-feet/enterprise/entities/value-objects/cpf'
+import { DatabaseModule } from '@/infra/database/database.module'
 import { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
 import { hash } from 'bcryptjs'
 import request from 'supertest'
+import { AccountFactory } from 'test/factories/make-employee'
 import { AppModule } from '../../app.module'
 import { PrismaService } from '../../database/prisma/prisma.service'
 
 describe('Authenticate (E2E)', () => {
   let app: INestApplication
   let prisma: PrismaService
+  let accountFactory: AccountFactory
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [AccountFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
     prisma = moduleRef.get(PrismaService)
+    accountFactory = moduleRef.get(AccountFactory)
     await app.init()
   })
 
   test('[POST] /sessions', async () => {
-    await prisma.accounts.create({
-      data: {
-        name: 'admin',
-        email: 'admin@email.com',
-        cpf: '12345678900',
-        password: await hash('123456', 8),
-        role: 'admin',
-      },
+    await accountFactory.makePrismaEmployee({
+      cpf: new CPF('12345678900'),
+      password: await hash('123456', 8),
     })
 
     const response = await request(app.getHttpServer()).post('/sessions').send({
--- test/factories/make-employee.ts
+++ test/factories/make-employee.ts
@@ -4,7 +4,10 @@ import {
   type EmployeeProps,
 } from '@/domain/fast-feet/enterprise/entities/employee'
 import { CPF } from '@/domain/fast-feet/enterprise/entities/value-objects/cpf'
+import { PrismaEmployeeMapper } from '@/infra/database/prisma/mappers/prisma-employee-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 import { generateCPF } from 'test/utils/generate-cpf'
 
 export function makeEmployee(
@@ -24,4 +27,21 @@ export function makeEmployee(
   )
 
   return employee
+}
+
+@Injectable()
+export class AccountFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaEmployee(
+    data: Partial<EmployeeProps> = {}
+  ): Promise<Employee> {
+    const account = makeEmployee(data)
+
+    await this.prisma.accounts.create({
+      data: PrismaEmployeeMapper.toPrisma(account),
+    })
+
+    return account
+  }
 }
