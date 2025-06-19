7cf74ee7d2513a05704cc5cd5214c230d4e5b610
Jhone-cmd <jhone.1941@gmail.com>
2025-06-18 22:25:22 -0300

    chore: nest status use case on order updated

--- src/infra/http/nest-use-cases/nest-on-order-status-use-case.ts
+++ src/infra/http/nest-use-cases/nest-on-order-status-use-case.ts
@@ -1,10 +1,14 @@
+import { OrderAttachmentRepository } from '@/domain/fast-feet/application/repositories/order-attachment-repository'
 import { OrderRepository } from '@/domain/fast-feet/application/repositories/order-repository'
 import { OnOrderStatusUseCase } from '@/domain/fast-feet/application/use-cases/on-order-status'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class NestOnOrderStatusUseCase extends OnOrderStatusUseCase {
-  constructor(orderRepository: OrderRepository) {
-    super(orderRepository)
+  constructor(
+    orderRepository: OrderRepository,
+    orderAttachmentRepository: OrderAttachmentRepository
+  ) {
+    super(orderRepository, orderAttachmentRepository)
   }
 }
